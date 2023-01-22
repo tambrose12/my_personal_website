1dec5c1d1895cd707472071bd1448d048f5ca41c
Taylor Ambrose <taykesby@gmail.com>
2023-01-21 19:32:38 -0500

    added js event listener for button, and button hover effect

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ const h2 = document.createElement("h2");
 h2.textContent = "Thanks for reading a little bit about me!";
 
 document.querySelector("body").appendChild(h2);
+
+
+const input = document.getElementById('button');
+
+function clickAlert() {
+    alert('Thanks for stopping by!');
+}
+
+input.addEventListener('click', clickAlert);
