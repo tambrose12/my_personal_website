b7384e2eb09b58a5ac72ff7bd7a7635cf0ab7b41
Taylor Ambrose <taykesby@gmail.com>
2023-01-21 19:19:03 -0500

    added div backgrounds and a button, styled divs and button

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Thanks for reading a little bit about me!";
 
 document.querySelector("body").appendChild(h2);
